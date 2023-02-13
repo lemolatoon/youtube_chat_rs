42cee10689efb95285a55c2e1d6d9a9608aac7d7
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-14 01:54:55 +0900

    Add tests for LiveClientBuilder

--- src/live_chat.rs
+++ src/live_chat.rs
@@ -7,7 +7,9 @@ struct LiveChatClient {
 }
 
 impl LiveChatClient {
-    pub fn start(&self) {}
+    pub fn start(&self) {
+        unimplemented!()
+    }
 }
 
 struct Empty;
@@ -18,14 +20,22 @@ pub struct LiveChatClientBuilder<T> {
 }
 
 impl LiveChatClientBuilder<Empty> {
-    pub fn live_id(live_id: String) -> LiveChatClientBuilder<FullFilled> {
+    pub fn new() -> LiveChatClientBuilder<Empty> {
+        Self {
+            live_url: None,
+            __live_id_state: PhantomData::<Empty> {},
+        }
+    }
+
+    pub fn live_id(&self, live_id: String) -> LiveChatClientBuilder<FullFilled> {
         LiveChatClientBuilder {
-            live_url: Some(live_id),
+            live_url: Some(format!("https://www.youtube.com/watch?v={}", live_id)),
             __live_id_state: PhantomData::<FullFilled> {},
         }
     }
 
     pub fn url(
+        &self,
         raw_url: impl AsRef<str>,
     ) -> Result<LiveChatClientBuilder<FullFilled>, anyhow::Error> {
         Url::parse(raw_url.as_ref())?;
@@ -35,7 +45,7 @@ impl LiveChatClientBuilder<Empty> {
         })
     }
 
-    pub fn channel_id(channel_id: String) -> LiveChatClientBuilder<FullFilled> {
+    pub fn channel_id(&self, channel_id: String) -> LiveChatClientBuilder<FullFilled> {
         LiveChatClientBuilder::<FullFilled> {
             live_url: Some(format!(
                 "https://www.youtube.com/channel/{}/live",
@@ -51,5 +61,38 @@ impl LiveChatClientBuilder<FullFilled> {
         LiveChatClient {
             live_url: self.live_url.unwrap(), // never fails
         }
+    }
+}
+
+#[cfg(test)]
+mod live_chat_tests {
+    use super::*;
+
+    #[test]
+    fn test_builder() {
+        let client = LiveChatClientBuilder::new()
+            .live_id("_eM9C3zZL14".to_string())
+            .build();
+        assert_eq!(
+            &client.live_url,
+            "https://www.youtube.com/watch?v=_eM9C3zZL14"
+        );
+
+        let client = LiveChatClientBuilder::new()
+            .channel_id("UCHVXbQzkl3rDfsXWo8xi2qw".to_string())
+            .build();
+        assert_eq!(
+            &client.live_url,
+            "https://www.youtube.com/channel/UCHVXbQzkl3rDfsXWo8xi2qw/live"
+        );
+
+        let client = LiveChatClientBuilder::new()
+            .url("https://www.youtube.com/watch?v=Dx5qFachd3A".to_string())
+            .unwrap()
+            .build();
+        assert_eq!(
+            &client.live_url,
+            "https://www.youtube.com/watch?v=Dx5qFachd3A"
+        );
     }
 }
